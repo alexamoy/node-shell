dab5e19f772ebe40053e0354498ea05e183ea3a1
Lara Pollack <larpollack@gmail.com>
2018-03-05 11:56:18 -0500

    completed thru modules

--- bash.js
+++ bash.js
@@ -2,16 +2,15 @@
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-  var date = new Date();
-    if (cmd==='pwd'){
-        process.stdout.write(process.cwd());
-    }
-    if (cmd==='date'){
-        process.stdout.write(date.toUTCString());
-    }
-  process.stdout.write('\nprompt > ');
+var commands = require('./commands');
 
-});
+
+process.stdin.on('data', function (data) {
+      var cmd = data.toString().trim(); // remove the newline
+      commands[cmd]();
+        
+        
+        
+      // process.stdout.write('\nprompt > ');
+})
 //console.log(Object.keys(process))
